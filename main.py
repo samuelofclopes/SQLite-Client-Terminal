970c0712d2dc626ff665e3637d145d2d81070197
Moita <samuel.ofc.lopes@gmail.com>
2026-04-11 14:44:34 +0100

    Código base, versão de teste

---
+++ main.py
@@ -0,0 +1,17 @@
+import sqlite3
+from interface import user_command, user_file
+
+def conexao(user_f, user_c):
+    dados = None
+    try:
+        conn = sqlite3.connect(user_f)
+        cursor = conn.cursor()
+        cursor.execute(str(user_c))
+        if user_c.upper().startswith(("INSERT", "UPDATE", "DELETE", "CREATE", "DROP", "ALTER", "REPLACE")):
+            conn.commit()
+        else:
+            dados = cursor.fetchall()
+        cursor.close()
+        conn.close()
+    except sqlite3.Error as e: return str(e)
+    return True, dados
